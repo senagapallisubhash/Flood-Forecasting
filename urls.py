cac610a9b2e9d91095e919d80264473da207a576
Subhash <senagapallisubhash@gmail.com>
2025-04-16 15:13:22 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,22 +1,14 @@
-"""FloodForecast URL Configuration
+from django.urls import path
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
+from . import views
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('FloodForecastApp.urls')),
+urlpatterns = [path("index.html", views.index, name="index"),
+			path("Signup.html", views.Signup, name="Signup"),
+			path("SignupAction", views.SignupAction, name="SignupAction"),	    	
+			path("UserLogin.html", views.UserLogin, name="UserLogin"),
+			path("UserLoginAction", views.UserLoginAction, name="UserLoginAction"),
+			path("ProcessData", views.ProcessData, name="ProcessData"),
+			path("TrainML", views.TrainML, name="TrainML"),
+			path("PredictAction", views.PredictAction, name="PredictAction"),
+			path("Forecast", views.Forecast, name="Forecast"),
 ]
